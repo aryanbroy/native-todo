38485a98ccd9175df10bf48dff981e8309cafbb6
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-10-20 01:28:51 +0530

    dummy header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,52 @@
+import { createSettingsStyles } from '@/assets/styles/settings.styles';
 import useTheme from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
 import { Text, TouchableOpacity, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+  const styles = createSettingsStyles(colors);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={styles.container}
     >
-      <Text>Home page Niga</Text>
+      <View style={styles.header}>
+        <View style={styles.titleContainer}>
+          <LinearGradient
+            colors={colors.gradients.primary}
+            style={styles.iconContainer}
+          >
+            <Ionicons name="flash-outline" size={30} color={'#ffffff'} />
+          </LinearGradient>
+          <View>
+            <Text style={styles.title}>Today&apos;s Task</Text>
+            <Text style={styles.statLabel}>2 out of 4 completed</Text>
+          </View>
+        </View>
+      </View>
       <TouchableOpacity onPress={() => toggleDarkMode()}>
         <Text>Toggle Mode</Text>
       </TouchableOpacity>
-    </View>
+    </LinearGradient>
   );
 }
+
+// const styles = StyleSheet.create({
+//   view: {
+//     flex: 1,
+//     // justifyContent: 'center',
+//     // alignItems: 'center',
+//   },
+//   header: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     // justifyContent: 'center',
+//   },
+//   heading: {
+//     fontSize: 45,
+//     fontWeight: 'bold',
+//   },
+// });
